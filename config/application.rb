eaa030c757be7a702bd7b3a91c8d28c75f163010
nbk1510 <nbk1510@gmail.com>
2019-04-19 16:38:37 +0700

    fb

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,7 @@ module Bookstore2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.assets.initialize_on_precompile = false
-    config.assets.js_compressor = Uglifier.new(harmony: true)
+    # config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
